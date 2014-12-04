f5f929031135ca2c40357db30441a556ea545ad7
KamilahJ4e <kamilahjae@gmail.com>
2014-12-04 11:08:18 -0800

    working homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
   def index
-    @weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial").parsed_response
-    @forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=imperial&cnt=6").parsed_response["weatherdata"]["forecast"]["time"]
+    # @weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial").parsed_response
+    # @forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=imperial&cnt=6").parsed_response["weatherdata"]["forecast"]["time"]
   end
 
 end

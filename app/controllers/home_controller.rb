77e20a3725cc8cd97325e92d23eb7206fb985ae6
Kat <kmmpatke@gmail.com>
2014-12-03 16:33:11 -0800

    made 5-day forecast into a loop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial").parsed_response
-    @forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=imperial&cnt=5").parsed_response["weatherdata"]["forecast"]["time"]
+    @forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=imperial&cnt=6").parsed_response["weatherdata"]["forecast"]["time"]
   end
 
 end

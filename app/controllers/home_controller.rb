44d90ed074a93754ae6556af65fb888bb02ceb66
Kat <kmmpatke@gmail.com>
2014-12-03 16:04:39 -0800

    added current weather to front page, mid-way through adding forecast

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+
+  def index
+    @weather = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial").parsed_response
+    @forecast = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=xml&units=imperial&cnt=5").parsed_response["weatherdata"]["forecast"]["time"]
+  end
+
 end

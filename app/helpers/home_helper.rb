4262b7b1100eac87d804b00ecb1c11f92a9037a1
Kat <kmmpatke@gmail.com>
2014-12-03 16:15:33 -0800

    added helper method to convert mps to mph for windspeed

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+
+  def convert_wind_to_mph(mps_wind)
+  #  1 Meter per Second = 2.2369362920544 Miles per Hour
+    mph_wind = mps_wind * 2.2369362920544
+    mph_wind.to_i
+  end
+
 end

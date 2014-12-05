e674e485e82ddf99715419345aea6129c1930a0c
Kat <kmmpatke@gmail.com>
2014-12-05 14:46:53 -0800

    made user form a partial, added log in/log out flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
     if login.empty?
       @current_user = User.create(uid: auth_hash[:uid], provider: auth_hash[:provider], pic: auth_hash[:info][:image], name: auth_hash[:info][:nickname])
       session[:id] = @current_user.id
-      redirect_to moar_login_path(@current_user.id)
+      redirect_to moar_login_path(@current_user.id), notice: "Welcome to HoneyDew, <%=@current_user.name %>!"
     else
       session[:id] = login.first.id
-      redirect_to root_path
+      redirect_to root_path, notice: "Welcome back!"
     end
   end
 
   def logout
     session[:id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: "Successfully logged out."
   end
 
 

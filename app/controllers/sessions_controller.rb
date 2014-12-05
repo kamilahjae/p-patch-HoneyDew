e6a2f18a0b50b0d0d2992de53a797e96bfa4389a
Kat <kmmpatke@gmail.com>
2014-12-04 16:35:27 -0800

    user is now saved to database when logging in with twitter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,28 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-  redirect_to root_path
+    login = User.where(uid: auth_hash[:uid], provider: auth_hash[:provider])
+    if login.empty?
+      User.create(uid: auth_hash[:uid], provider: auth_hash[:provider], pic: auth_hash[:info][:image], name: auth_hash[:info][:nickname])      
+    end
+    redirect_to root_path
   end
 
+  # auth_hash = request.env["omniauth.auth"]
+  # login = Authentication.where(uid: auth_hash[:uid], provider: auth_hash[:provider])
+  # if current_bro
+  #   if login.empty?
+  #     current_bro.authentications.create(provider: auth_hash[:provider], uid: auth_hash[:uid])
+  #     redirect_to show_path(current_bro.id), notice: "Authentication added"
+  #   else
+  #     redirect_to show_path(current_bro.id), notice: "Account already in use!"
+  #   end
+  # else
+  #   if login.empty?
+  #     login << make_bro_and_auth(auth_hash)
+  #     # If empty, create a new Authentication/User
+  #   end
+  #   session[:bro_id] = login.first.user.id
+  #   redirect_to root_path, notice: "So stoked to see you, bro"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141204000423) do
+ActiveRecord::Schema.define(version: 20141205001933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20141204000423) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "uid"
+    t.string   "provider"
+    t.text     "pic"
   end
 
 end

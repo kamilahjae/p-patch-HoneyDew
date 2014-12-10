2e9833c819a9f7ff5f21eebbec20194bdad7403e
Kat <kmmpatke@gmail.com>
2014-12-10 11:31:36 -0800

    did some stuff

--- app/controllers/tool_sheds_controller.rb
+++ app/controllers/tool_sheds_controller.rb
@@ -4,8 +4,9 @@ class ToolShedsController < ApplicationController
     user_id: params[:user_id],
     tool_id: params[:tool_id]
     )
-    @toolshed.save
-
+    if @toolshed.save
+      redirect_to tools_path, notice: "You have successfully reserved a tool."
+    end
     # to hide the user and too id in the route, create a form with 2 hidden fields on the tool botton
   end
 end

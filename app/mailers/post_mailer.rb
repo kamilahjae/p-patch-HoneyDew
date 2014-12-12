d914eb465c28219df8869bdbc4f1e653e1bd952b
Kat <kmmpatke@gmail.com>
2014-12-12 11:32:42 -0800

    posts now linked to mandrill email app

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -3,7 +3,17 @@ class PostMailer < ActionMailer::Base
 
   def new_post(post_id)
     @post = Post.find(post_id)
-    mail(to: "regalbee400@gmail.com", subject: "A new HoneyDew blog post has been added!")
+    @users = User.all
+    @email_array = []
+    @users.where("email IS NOT NULL").each do |user|
+        @email_array << user.email
+    end
+
+    mail(cc:
+      @email_array.each do |email|
+        email
+      end,
+      subject: "A new HoneyDew blog post has been added!")
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.mandrillapp.com',
     port:                 587,
     domain:               'honey-dew.herokuapp.com',
     user_name:            ENV["MAIL_USERNAME"], # your gmail login
